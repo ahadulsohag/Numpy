8938b39fd50145731f02f21768642e87a5aab86c
Sohag <ahadulsohag@gmail.com>
2026-03-01 23:02:45 +0600

    Update slicing.py

--- slicing.py
+++ slicing.py
@@ -1,3 +1,5 @@
 import numpy as np
-array = np.array([[-2,-1,0], [1,2,3], [4,5,6]])
-print(array[0:2,0:2])
+array = np.array([[-2,-1,0],
+                  [1,2,3],
+                  [4,5,6]])
+print(array[1:,1:])
