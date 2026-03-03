a4f7e1f9f787d2b426771fa04ab90a68e9be020e
Sohag <ahadulsohag@gmail.com>
2026-03-03 20:48:01 +0600

    practice node

--- ndarray.py
+++ ndarray.py
@@ -1,6 +1,8 @@
 import numpy as np
+import numpy
 array = np.array([[['a','b','c'],['d','e','f'],['g','h','i']],
                   [['j','k','l'],['m','n','o'],['p','q','r']],
                   [['s','t','u'],['v','w','x'],['y','z',' ']]])
 print(array.ndim)
+print()
 print(array.shape)
