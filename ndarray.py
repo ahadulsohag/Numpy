fbc6d61dd15da5906ed8c87ac845327b704b92d4
Sohag <ahadulsohag@gmail.com>
2026-03-03 20:42:57 +0600

    added

--- ndarray.py
+++ ndarray.py
@@ -1,9 +1,6 @@
 import numpy as np
-array = np.array([[['a','b','c', 'd'],
-                  ['e','f','g', 'h']],
-                  [['i','j','k', 'l'],
-                  ['l','m','n', 'o']],
-                  [['p','q','r', 's'],
-                  ['t','u','v', 'w']]])
+array = np.array([[['a','b','c'],['d','e','f'],['g','h','i']],
+                  [['j','k','l'],['m','n','o'],['p','q','r']],
+                  [['s','t','u'],['v','w','x'],['y','z',' ']]])
 print(array.ndim)
 print(array.shape)
