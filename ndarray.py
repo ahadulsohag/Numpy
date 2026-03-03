638f5b18a422f84e06e54e50fce368b3d663e478
Sohag <ahadulsohag@gmail.com>
2026-03-03 20:33:27 +0600

    checking

---
+++ ndarray.py
@@ -0,0 +1,9 @@
+import numpy as np
+array = np.array([[['a','b','c', 'd'],
+                  ['e','f','g', 'h']],
+                  [['i','j','k', 'l'],
+                  ['l','m','n', 'o']],
+                  [['p','q','r', 's'],
+                  ['t','u','v', 'w']]])
+print(array.ndim)
+print(array.shape)
